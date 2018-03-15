fa241244082d0ca27de191186b413383f89383c0
Casper Faelens <caspfael@student.arteveldehs.be>
2018-03-15 12:10:18 +0100

    oef

--- week 4/oefening 2/webdadi-praktijk-4-rob-de-bouwer/scripts/custom.js
+++ week 4/oefening 2/webdadi-praktijk-4-rob-de-bouwer/scripts/custom.js
@@ -19,7 +19,6 @@ return brick;
 function addBrickToWall(color, wallNumber) {
     //<div id="wall-1" class"wall">
     let wallId= 'wall-' + wallNumber;
-
     //element ophalen (juiste muur ophalen)
     let wall = document.getElementById(wallId);
     //steentje maken voor de muur
@@ -33,7 +32,73 @@ let wall = document.getElementById('wall-'+wallNumber);
 
 let bricks= document.getElementsByClassName('brick-'+color);
 if(bricks.length > 0){
-    wall.removeChild(bricks[0]);
+    wall.removeChild(bricks[0]); 
+    if (bricks.length==0) {
+        alert('De muur is volledig gesloopt');
+    }  
+}    
 }
+let amountYellow = document.getElementsByClassName('brick-yellow');
+let amountBlack = document.getElementsByClassName('brick-black');
+let amountOrange = document.getElementsByClassName('brick-orange');
+let amountBlue = document.getElementsByClassName('brick-blue');
+let amountBricks = [amountYellow.length,amountBlack.length,amountOrange.length,amountBlue.length];
+
+function helpRobCalculate (){
+    //aantal stenen bepalen
+    if (amountYellow.length==1) {
+        document.getElementById('wall-1-amountstones').innerHTML='1 steen'
+    } else {
+        document.getElementById('wall-1-amountstones').innerHTML=amountYellow.length + ' stenen'
+    }
+
+    if (amountBlack.length==1) {
+        document.getElementById('wall-2-amountstones').innerHTML='1 steen'
+    } else {
+        document.getElementById('wall-2-amountstones').innerHTML=amountBlack.length + ' stenen'
+    }
+
+    if (amountBlack.length==1) {
+        document.getElementById('wall-3-amountstones').innerHTML='1 steen'
+    } else {
+        document.getElementById('wall-3-amountstones').innerHTML=amountOrange.length + ' stenen'
+    }
+    
+    if (amountBlack.length==1) {
+        document.getElementById('wall-4-amountstones').innerHTML='1 steen'
+    } else {
+        document.getElementById('wall-4-amountstones').innerHTML=amountBlue.length + ' stenen'
+    }
+//kostprijs bepalen
+    let costYellow=(amountYellow.length*2)
+    let costBlack=(amountBlack.length*7.5)
+    let costOrange=(amountOrange.length*9)
+    let costBlue=(amountBlue.length*0.30)
+    document.getElementById('cost-yellow').innerHTML='€ '+ (amountYellow.length*2);
+    document.getElementById('cost-black').innerHTML='€ '+ (amountBlack.length*7.5);
+    document.getElementById('cost-orange').innerHTML='€ '+ (amountOrange.length*9);
+    document.getElementById('cost-blue').innerHTML='€ '+ (amountBlue.length*0.30);
+    document.getElementById('cost-total').innerHTML='€ '+(costYellow + costBlack +costOrange +costBlue);
+//de grootste bepalen
+    if (Math.max(amountBricks=amountYellow.length)) {
+     document.getElementById('wall-biggest') .innerHTML='De gele muur is het grootst.'  
+     if (Math.max(amountBricks=amountBlack.length)) {
+        document.getElementById('wall-biggest') .innerHTML='De zwarte muur is het grootst.'  
+        if (Math.max(amountBricks=amountOrange.length)) {
+            document.getElementById('wall-biggest') .innerHTML='De oranje muur is het grootst.' 
+            if (Math.max(amountBricks=amountBlue.length)) {
+                document.getElementById('wall-biggest') .innerHTML='De blauwe muur is het grootst.'  
+               } 
+           }
+       }
+    }
+    
+    
     
 }
+
+
+
+
+
+    
---
+++ week 4/oefening 3/webdadi-praktijk-5-happy-tree-friends-memory-game/js/index.js
@@ -0,0 +1,9 @@
+
+
+function showBlock(block) {
+    for (let block = document.getElementsByClassName("memory-block"); block < array.length; block++) {
+        const element = array[block];
+        
+    }
+    block.style.opacity="";
+}
